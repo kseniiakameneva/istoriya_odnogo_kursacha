b1b4afbf13da9b814a7b7295cff5bfcbcd73c348
kseniiakameneva <boboshka111@gmail.com>
2018-05-28 13:11:03 +0300

    search field is added'

--- prokat/forms.py
+++ prokat/forms.py
@@ -1,17 +1,14 @@
 from django import forms
-from .models import Order
+from .models import Order, Product
 from django.utils import timezone
 from django.contrib.admin import widgets
 from django.contrib.admin.widgets import AdminDateWidget
 from django.forms.fields import DateField
 
 
-
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('product', 'name', 'phone', 'comment', 'date', 'date_end')
-
-
 
 
--- prokat/views.py
+++ prokat/views.py
@@ -1,10 +1,11 @@
+#-*- coding: utf-8 -*-
 from django.shortcuts import render, get_object_or_404
 from django.db import connection
 from prokat.models import Category, Product, Type
 from .forms import BookingForm
+from django.views.generic import ListView
+from django.db.models import Q
 from django.shortcuts import redirect
-from django.db.models.expressions import RawSQL
-from django.db.models.query import QuerySet
 
 
 def base_view(request):
@@ -20,9 +21,10 @@ def base_view(request):
 
 
 def prod_list(request, pk):
-    #products = Product.objects.filter(category=pk, available=True)
-    products = Product.objects.raw("SELECT * FROM prokat_product WHERE (prokat_product.available = 1 AND prokat_product.category_id = %s)",(pk))
-   # print(products.query)
+    # products = Product.objects.filter(category=pk, available=True)
+    products = Product.objects.raw(
+        "SELECT * FROM prokat_product WHERE (prokat_product.available = 1 AND prokat_product.category_id = %s)", (pk))
+    # print(products.query)
     categories = Category.objects.raw('SELECT * FROM prokat_category ')
     context = {
         'categories': categories,
@@ -60,3 +62,26 @@ def booking_view(request):
     else:
         form = BookingForm()
     return render(request, 'booking.html', {'form': form})
+
+
+def get_queryset(request):
+    # Получаем не отфильтрованный кверисет всех моделей
+    queryset = Product.objects.all()
+    q = request.GET.get("q")
+
+    if q != "":
+        # Если 'q' в GET запросе, фильтруем кверисет по данным из 'q'
+        # products = queryset.filter(Q(title__icontains=q) |
+        # Q(type__icontains=q))
+        q1=q.title()
+        id_type = Type.objects.filter(type__contains=q1)
+        categories = Category.objects.raw('SELECT * FROM prokat_category ')
+        products = Product.objects.filter(Q(title__contains=q1) | Q(type = id_type ))
+            #"SELECT * FROM prokat_product WHERE prokat_product.title LIKE '%%%s%%' OR prokat_product.type_id =%s ", (q, q))
+        context = {
+            'categories': categories,
+            'products': products,
+            'q': q
+        }
+        return render(request, 'place_search.html', context)
+    return render(request, 'base.html')
